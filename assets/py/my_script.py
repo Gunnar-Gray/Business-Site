11c52864ae6ce584c3ff4bb55125dfd3cb4c1c25
Gunnar-Gray <gunnargray41@gmail.com>
2023-04-29 11:08:07 -0400

    python

---
+++ assets/py/my_script.py
@@ -0,0 +1,7 @@
+import os
+import openai
+
+# Replace the API key with your own
+openai.api_key = "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"
+
+# Your code for working with the OpenAI API
